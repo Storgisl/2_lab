3d09812bd13cc1db8cc31e53db30550d58ad4dab
Storgisl <91078979+Storgisl@users.noreply.github.com>
2024-02-05 13:17:02 +0300

    commit

---
+++ model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# class DecisionTreeClassifier:
+#     def __init__(self, x):
+#         self.x = x
+
+#     def
+
+df = pd.read_csv("data/Грибы.txt", sep="\t", encoding="Windows 1251")
